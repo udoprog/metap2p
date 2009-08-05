019c976966c1d80e84a5315b46b95266f3acd0df
John-John Tedro <udoprog@patty.(none)>
2009-08-05 16:04:43 +0200

    Fixed some major unicode issues when passing POST or GET parameters.

--- metap2p_service/config/router.py
+++ metap2p_service/config/router.py
@@ -3,5 +3,7 @@ import routes
 def setup_router(m):
   m.connect('/', controller="base", action="index", conditions=dict(method=['GET']))
   m.connect('peers', '/peers', controller="peers", action="index", conditions=dict(method=['GET']))
+  m.connect('/peers', controller="peers", action="create", conditions=dict(method=['POST']))
+  m.connect('new_peer', '/peers/new', controller="peers", action="new", conditions=dict(method=['GET']))
   m.connect('show_peer', '/peers/:peer_uri', controller="peers", action="show", conditions=dict(method=['GET']))
   return m
--- metap2p_service/controllers/peers.py
+++ metap2p_service/controllers/peers.py
@@ -8,7 +8,8 @@ class Peers(Controller):
       T.head()[
         T.title()["MetaP2P Control Panel (%s)"%(self.server.uuid.hex)],
         T.link_to_css("/public/css/master.css"),
-        T.link_to_javascript("/public/js/master.js")
+        T.link_to_javascript("/public/js/master.js"),
+        T.meta({'http-equiv': "content-type", 'content': "text/html; charset=utf-8"})
       ],
       T.body()[
         T.div(id="heading")[
@@ -31,7 +32,7 @@ class Peers(Controller):
     self.path=["Peers"]
 
     peerlist = list()
-
+    
     peerlist.append(T.div(_class="row heading")[
       T.span(_class="name")["host"],
       T.span(_class="transmit")["tX (out)"],
@@ -47,7 +48,7 @@ class Peers(Controller):
           uuid = "<none>"
 
         peerlist.append(T.div(_class="row")[
-          T.span(_class="name")[T.link_to(url_for(action="show", peer_uri=peer.uri))[peer.uri]],
+          T.span(_class="name")[T.link_to(url_for(action="show", peer_uri=peer.uri))[peer.host]],
           T.span(_class="transmit")[str(peer.session.tx)],
           T.span(_class="receive")[str(peer.session.rx)],
           T.span(_class="uuid")[uuid],
@@ -55,12 +56,13 @@ class Peers(Controller):
         ])
       else:
         peerlist.append(T.div(_class="row")[
-          T.span(_class="name")[T.link_to(url_for(action="show", peer_uri=peer.uri))[peer.uri]],
+          T.span(_class="name")[T.link_to(url_for(action="show", peer_uri=peer.uri))[peer.host]],
           "not connected",
           T.div(_class="clear")
         ])
     
-    return T.div(id="peers")[peerlist]
+    return [T.div(_class="mininav")[T.span(_class='item')[T.link_to(url_for('new_peer'))["new"]]],
+            T.div(id="peers")[peerlist]]
   
   def show(self, peer_uri):
     import urllib
@@ -70,3 +72,17 @@ class Peers(Controller):
       T.h1()[peer_uri],
       T.p()["This is a paragraph"]
     ]
+
+  def new(self):
+    self.path = ["Peers", "new"]
+    
+    return [
+      T.form(action=url_for('peers'), method="POST")[
+        T.input(name="peer_uri", type="text"),
+        T.input(name="new_peer", type="submit", value="New Peer")
+      ]
+    ]
+  
+  def create(self, peer_uri):
+    self.server.addPeer(peer_uri)
+    redirect_to(url_for(action="index"))
