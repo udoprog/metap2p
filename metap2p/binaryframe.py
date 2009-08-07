7a47e713766b4447f6eb2d11d9346e7872bfa8ff
John-John Tedro <udoprog@patty.(none)>
2009-08-07 13:03:23 +0200

    New binaryframes that can handle more sheizze.

--- metap2p/binaryframe.py
+++ metap2p/binaryframe.py
@@ -1,27 +1,43 @@
 import struct
 
 class Field(object):
-  __instance_counter = 0
+  # must be set for this to work properly.
   __name__ = None
+  # is used in order to sort the fields properly
+  __instance_counter = 0
+  
+  def __init__(self, *args, **kw):
 
-  def __init__(self, struct, default=None):
-    self.struct = struct
-    self.id = id(self)
-    self.default = default
-    
-    self.__instance = self.__class__.__instance_counter
+    if len(args) == 1:
+      self.struct = args[0]
+      self._inherit_struct = False
+    else:
+      self._inherit_struct = True
+
+    self._instance = self.__class__.__instance_counter
     self.__class__.__instance_counter += 1
+    
+    self.default = kw.get('default')
   
   def __set__(self, obj, val):
-    obj.__store__[self.id] = val
+    if obj is None:
+      return None
+    
+    obj.__store__[self.__name__] = val
   
   def __get__(self, obj, objtype):
-    if self.id in obj.__store__:
-      return obj.__store__[self.id]
-    return self.default
+    if obj is None:
+      #class access
+      return self.default
+    
+    # instsance access
+    return obj.__store__[self.__name__]
   
   def __cmp__(self, other):
-    return cmp(self.__instance, other.__instance)
+    return cmp(self._instance, other._instance)
+  
+  def __str__(self):
+    return "<Field name='%s'>"%(self.__name__)
 
 class Frame(object):
   __used__ = False
@@ -36,28 +52,41 @@ class Frame(object):
   
   def __init__(self, **kw):
     self.__store__ = dict()
+    
+    for k in self.__class__.__fields__:
+      field = self.__class__.__fields__[k]
+      self.__store__[k] = field.default
+    
     for k in kw:
       setattr(self, k, kw[k])
+    
+    self._create_()
   
   def _digest_(self):
-    if not self.__class__.__used__:
-      return None
-
-    for field in self.__class__.__fields__:
+    for field in self.__class__.__ordered_fields__:
       if field.__get__(self, None) is None:
-        raise Exception("Field %s:%d is None"%(field.__name__, field.__instance))
+        raise Exception("%s: Field '%s' is None"%(repr(self), field.__name__))
     
-    return struct.pack(self.__class__.__frame__, *map(lambda field: field.__get__(self, None), self.__class__.__fields__))
+    return struct.pack(self.__class__.__frame__, *map(lambda field: field.__get__(self, None), self.__class__.__ordered_fields__))
   
   def _feed_(self, data):
-    if not self.__class__.__used__:
-      return
-
-    for field, arg in zip(self.__class__.__fields__, struct.unpack(self.__class__.__frame__, data)):
+    if len(data) < self._size_():
+      raise Exception("input not long enough")
+    
+    if len(data) != self._size_():
+      data = buffer(data, 0, self._size_())
+    
+    for field, arg in zip(self.__class__.__ordered_fields__, struct.unpack(self.__class__.__frame__, data)):
       field.__set__(self, arg)
+    
+    return self
   
   def _size_(self):
     return self.__class__.__size__
+  
+  def _create_(self):
+    pass
+    #self.size = self._size_();
   
   @classmethod
   def setup_frame(cls):
@@ -66,28 +95,35 @@ class Frame(object):
 
     If not marked as used; All values will be None, digest will return None.
     """
-    cls.__fields__ = list()
+    cls.__fields__ = dict()
+    
+    for f in cls.__dict__:
+      attr = cls.__dict__[f]
+      if isinstance(attr, Field):
+        attr.__name__ = f
+        cls.__fields__[f] = attr
+    
+    for super in cls.__bases__:
+      for f in super.__dict__:
+        attr = super.__dict__[f]
+        
+        if isinstance(attr, Field):
+          if f not in cls.__fields__:
+            cls.__fields__[f] = attr
+            continue
+          
+          field = cls.__fields__[f]
+          
+          if field._inherit_struct:
+            field.struct = attr.struct
+            field._inherit_struct = False
+            field._instance = attr._instance
 
-    # grab all subclasses of this class
-    classes = list()
-    classes.append(cls)
-    for subclass in cls.__bases__:
-      classes.append(subclass)
-    
-    # grab all fields defined
-    for klass in classes:
-      for k in klass.__dict__:
-        if isinstance(klass.__dict__[k], Field):
-          klass.__dict__[k].__name__ = k
-          cls.__fields__.append(klass.__dict__[k])
-    
-    cls.__fields__.sort()
-    cls.__frame__ = ''.join(map(lambda field: field.struct, cls.__fields__))
-    cls.__used__ = True
+    cls.__ordered_fields__ = cls.__fields__.values()
+    cls.__ordered_fields__.sort()
+
+    cls.__frame__ = ''.join(map(lambda f: f.struct, cls.__ordered_fields__))
     cls.__size__ = struct.calcsize(cls.__frame__)
-
-  def beforesend(self):
-    return True
 
 if __name__ == '__main__':
   ping1 = Ping(stage = 22)
